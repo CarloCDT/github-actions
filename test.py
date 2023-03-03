53e94e24b49c5419c05e5f3fcc5ecf1cc9e2868c
Carlo di Francescantonio Torrado <carlo.dt@hotmail.com>
2023-03-03 08:52:35 -0500

    Test GHA

--- test.py
+++ test.py
@@ -1,6 +1,30 @@
-def test_gha(n):
-    assert n == 10, 'Number is not 10'
+import pandas as pd
+from datetime import datetime
+
+def load_leaderboard(path='leaderboard/leaderboard.csv'):
+    return pd.read_csv(path)
+
+def add_score(leaderboard, user="test_user", score=0):
+    
+    new_score = pd.DataFrame.from_dict({'user': [user], 'score': [score], 'date': [datetime.utcnow()]})
+    leaderboard = pd.concat([leaderboard, new_score], axis=0)
+
+    # Only save top-10
+    leaderboard = leaderboard.sort_values(by=['score', 'date'], ascending=[0,1]).head(10).reset_index(drop=True)
+
+    return leaderboard
+
+def save_leaderboard(leaderboard, path='leaderboard/leaderboard.csv'):
+    leaderboard.to_csv(path, ignore_index=True)
+
+def update_readme(leaderboard):
+    f = open("README.md", "w")
+    f.write("# Leaderboard - Using GHA\n\n")
+    f.write(leaderboard.to_markdown())
+    f.close()
 
 if __name__ == "__main__":
-    test_gha(10)
-    print("Number is 10, great!")
+    leaderboard = load_leaderboard()
+    add_score(leaderboard, user='CarloCDT', score=0.5)
+    save_leaderboard(leaderboard)
+    update_readme(leaderboard)
